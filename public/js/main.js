781954da18f8c352a5aa294e75fb3ddff1f89ebb
giohgio <giooh.giooo15@gmail.com>
2025-10-03 19:56:03 -0300

    Update7 main.js

--- public/js/main.js
+++ public/js/main.js
@@ -14,7 +14,7 @@ button.addEventListener('click', async () => {
   }
 
   try {
-    const response = await fetch('https://servidor-rjr8.oregon.com/register', {
+    const response = await fetch('https://servidor-rjr8.onrender.com/register', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ identifier, senha })
